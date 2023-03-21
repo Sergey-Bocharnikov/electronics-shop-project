d7a6e40b4a025f844049c4b3e97112d68624fac1
Sergey-Bocharnikov <prokaznikov.s@yandex.ru>
2023-03-22 00:57:40 +0500

    tests - 100%

--- tests/test_item.py
+++ tests/test_item.py
@@ -15,8 +15,8 @@ def test_items_init(item_1):
 
 
 def test_calculate_total_price(item_1):
-    assert item_1.calculate_total_price == 200000.0
+    assert item_1.calculate_total_price() == 200000.0
 
 
-def test_apply_discount():
-    assert item_1.apply_discount == 8500.0
+def test_apply_discount(item_1):
+    assert item_1.apply_discount() == 8500.0
